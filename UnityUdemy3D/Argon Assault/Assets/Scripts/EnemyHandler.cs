b23879ed93015b3b1eef6bab254ebaea72ee582e
AnishAggarwal71 <anishaggarwal71@gmail.com>
2021-02-14 22:04:02 +0530

    Updated

---
+++ UnityUdemy3D/Argon Assault/Assets/Scripts/EnemyHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHandler : MonoBehaviour
+{
+    BoxCollider myBoxCollider;
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myBoxCollider = gameObject.AddComponent<BoxCollider>();
+        myBoxCollider.isTrigger = false;
+    }
+
+
+    private void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
---
+++ UnityUdemy3D/Argon Assault/Assets/Scripts/ScoreBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreBoard : MonoBehaviour
+{
+    [SerializeField] int scorePerHit = 12;
+    int score;
+
+    Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
+    }
+
+    public void HitScore()
+    {
+        score += scorePerHit;
+        scoreText.text = score.ToString();
+    }
+}
