a319b06261a3c339de94a1508f6bcce74acd13f1
AnishAggarwal71 <anishaggarwal71@gmail.com>
2021-02-17 15:56:10 +0530

    Updated

--- UnityUdemy3D/Argon Assault/Assets/Scripts/Player.cs
+++ UnityUdemy3D/Argon Assault/Assets/Scripts/Player.cs
@@ -78,27 +78,20 @@ public class Player : MonoBehaviour
     {
         if (CrossPlatformInputManager.GetButton("Fire"))
         {
-            ActivateGuns();
+            SetGunsActive(true);
         }
         else
         {
-            DeactivateGuns();
+            SetGunsActive(false);
         }
     }
 
-    private void ActivateGuns()
+    private void SetGunsActive(bool isActive)
     {
         foreach (GameObject gun in guns)
         {
-            gun.SetActive(true);
-        }
-    }
-
-    private void DeactivateGuns()
-    {
-        foreach(GameObject gun in guns)
-        {
-            gun.SetActive(false);
+            ParticleSystem.EmissionModule emissionModule = gun.GetComponent<ParticleSystem>().emission;
+            emissionModule.enabled = isActive;
         }
     }
 }
