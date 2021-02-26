63e0412a75d31df15e2e6326b83a9f45429551e4
AnishAggarwal71 <anishaggarwal71@gmail.com>
2021-02-26 15:24:06 +0530

    Edited Ardon Assult and Added Realm Rush

---
+++ UnityUdemy3D/Realm Rush/Assets/Scripts/EnemyMover.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMover : MonoBehaviour
+{
+    [SerializeField] List<Waypoint> path = new List<Waypoint>();
+    [SerializeField] float waitTime = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        foreach(Waypoint waypoint in path)
+        {
+            Debug.Log(waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
